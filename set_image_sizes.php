51c911613e67c26d83cda4dc3b21f052d46e704d
finfamiry <153509843+finfamiry@users.noreply.github.com>
2023-12-14 11:33:23 +0200

    Update set_image_sizes.php
    updated code

--- set_image_sizes.php
+++ set_image_sizes.php
@@ -1,62 +1,24 @@
 <?php
+add_filter('et_pb_portfolio_image_height', 'portfolio_size_h');
+add_filter('et_pb_portfolio_image_width', 'portfolio_size_w');
+function portfolio_size_h($height) {
+	return '400';
+}
+function portfolio_size_w($width) {
+	return '400';
+}
+add_image_size('custom-portfolio-size', 400, 400 );
 
-function custom_divi_image_sizes( $sizes ) {
-    $custom_sizes = array(
-        'custom-portfolio-size' => array(
-            'width' => '400',
-            'height' => '400',
-            'crop' => false
-        ),
-        'et-pb-post-main-image' => array(
-            'width' => '400',
-            'height' => '400',
-            'crop' => true
-        ),
-        'et-pb-post-main-image-fullwidth' => array(
-            'width' => '1080',
-            'height' => '400',
-            'crop' => true
-        ),
-        'et-pb-portfolio-module-image' => array(
-            'width' => '510',
-            'height' => '382',
-            'crop' => true
-        ),
-        'et-pb-portfolio-image-single' => array(
-            'width' => '1080',
-            'height' => '9999',
-            'crop' => false
-        ),
-        'et-pb-gallery-module-image-portrait' => array(
-            'width' => '400',
-            'height' => '516',
-            'crop' => true
-        ),
-        'et-pb-post-main-image-fullwidth-large' => array(
-            'width' => '2880',
-            'height' => '1800',
-            'crop' => true
-        ),
-        'et-pb-image--responsive--dektop' => array(
-            'width' => '1280',
-            'height' => '720',
-            'crop' => true
-        ),
-        'et-pb-image--responsive--table' => array(
-            'width' => '980',
-            'height' => '551',
-            'crop' => true
-        ),
-        'et-pb-image--responsive--phone' => array(
-            'width' => '480',
-            'height' => '270',
-            'crop' => true
-        ),
-    );
-    return array_merge( $sizes, $custom_sizes );
-};
+add_filter( 'et_pb_post_main_image_fullwidth_width', 'et_pb_post_main_image_width_customf' );
+add_filter( 'et_pb_post_main_image_fullwidth_height', 'et_pb_post_main_image_height_customf' );
+function et_pb_post_main_image_width_customf($width) {
+    return '1080';
+}
+function et_pb_post_main_image_height_customf($height) {
+    return '400';
+}
 
-add_filter( 'et_theme_image_sizes', 'custom_divi_image_sizes' );
+add_image_size( 'et-pb-post-main-image-fullwidth', 1080, 400, array( 'center', 'center' ) );
 
 function wpb_custom_image_sizes( $size_names ) {
     $new_sizes = array(
@@ -67,3 +29,5 @@ function wpb_custom_image_sizes( $size_names ) {
 }
 
 add_filter( 'image_size_names_choose', 'wpb_custom_image_sizes' );
+
+?>
